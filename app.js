53fedfcffa8db60c3684191d2c2d94a03ba30fbf
Ian <ian.shane.sears@gmail.com>
2020-02-09 11:23:12 -0700

    0.03 answer and start button hideable on js command

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
-// start timer
+// timer func
 function startTimer(duration, display) {
     // vars
     var timer = duration,
         minutes, seconds;
+// timer interval count down
     setInterval(function() {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
-
+        // min/sec countdown
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
+        // display for timer
         display.textContent = minutes + ":" + seconds;
-
+        // if timer runs out
         if (--timer < 0) {
             prompt('you done son!, enter your initials:')
         }
     }, 1000);
 }
 
+// timer start function
 window.onload = function() {
-    var fiveMinutes = 60 * 20,
+    var loadDisp = 60 * 20,
+    // push timer disp to span
         display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
+    startTimer(loadDisp, display);
 };
+
+const startBtn = document.getElementById('startBtn')
+const qContainerEl = document.getElementById('qContainer')
+
+startBtn.addEventListener('click', quizStart)
+
+function quizStart() {
+    startBtn.classList.add('hide')
+    qContainerEl.classList.remove('hide')
+}
+
+
+function nextQuest(){
+
+}
+
+
+function answerSelec(){
+
+}
