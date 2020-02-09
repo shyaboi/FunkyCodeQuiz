89cfde49dfc73a3c7df057e6957a30a170e6083b
Ian <ian.shane.sears@gmail.com>
2020-02-09 16:47:21 -0700

    0.04 answer, and question being appended with q/a array that was added

--- app.js
+++ app.js
@@ -26,23 +26,61 @@ window.onload = function() {
         display = document.querySelector('#time');
     startTimer(loadDisp, display);
 };
-
+// --------------------------------------------------------------------------------------------------------------
+// var dec
 const startBtn = document.getElementById('startBtn')
-const qContainerEl = document.getElementById('qContainer')
-
+const aContainerEl = document.getElementById('aContainer')
+var answerBtnsEl = document.getElementById('answerBtns')
+var qIndex
+var qEl = document.getElementById('question')
+let qShuffle
+// --------------------------------------------------------------------------------------------------------------
+// start button even listener
 startBtn.addEventListener('click', quizStart)
-
+// quiz start button function
 function quizStart() {
+    // hide start/next btn
     startBtn.classList.add('hide')
-    qContainerEl.classList.remove('hide')
+    nxtBtn.classList.add('hide')
+    // shuffle the question array
+    qShuffle = questions.sort(()=> Math.random() - 5)
+    // set question index
+    qIndex = 0
+    // remove hidden class on question and asnswers
+    aContainerEl.classList.remove('hide')
+// finally exe next questions function
+    nextQuest()
 }
 
-
+// next question function
 function nextQuest(){
+    qPop(qShuffle[qIndex])        
+    }
+// question and answer box text replacement
+function qPop(question){
+    qEl.innerText = question.question
+    question.answers.forEach(answer => {
+        let button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', answerSelec)
+        answerBtnsEl.appendChild(button)
+    })
+}
+
+function answerSelec(e){
 
 }
 
-
-function answerSelec(){
-
+var questions = [
+    {
+        question: 'question the first',
+        answers: [
+        {text:'yes', correct:true },
+        {text:'no', correct:false}
+    ]
 }
+]
