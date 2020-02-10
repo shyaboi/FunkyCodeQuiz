077ef88330a5511f7281b53f547a5a363d50ee07
Ian <ian.shane.sears@gmail.com>
2020-02-09 18:40:07 -0700

    0.05 questions added, initials on test array completion/timeout

--- app.js
+++ app.js
@@ -107,10 +107,47 @@ function correctStatus(){}
 
 var questions = [
     {
-        question: 'question the first',
+        question: 'Commonly used data types DO NOT include:',
         answers: [
-        {text:'yes', correct:true },
-        {text:'no', correct:false}
+        {text:'Strings', correct:false },
+        {text:'Booleans', correct:false},
+        {text:'Alerts', correct:true},
+        {text:'Numbers', correct:false}    
     ]
-}
+},
+{
+    question: 'The condition in an if/else statement is enclosed within___.',
+    answers: [
+    {text:'Quotes', correct:false },
+    {text:'Currly Brackets', correct:false},
+    {text:'Parentheses', correct:true},
+    {text:'square brackets', correct:false}    
+]
+},
+{
+    question: 'Arrays in JavaScript can be used to store___.',
+    answers: [
+    {text:'Numbers and strings', correct:true},
+    {text:'Other Arrays', correct:true},
+    {text:'Booleans', correct:true},    
+]
+},
+{
+    question: 'String values must be enclosed within__ when being assigned to variables.',
+    answers: [
+    {text:'Quotes', correct:true},
+    {text:'Currley Bois', correct:false},
+    {text:'Commas', correct:false },
+    {text:'Parentheses', correct:false}    
+]
+},
+{
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answers: [
+    {text:'Javascript', correct:false},
+    {text:'Terminal/Bash', correct:false},
+    {text:'For Loops', correct:false },
+    {text:'Console.log', correct:true}    
+]
+},
 ]
