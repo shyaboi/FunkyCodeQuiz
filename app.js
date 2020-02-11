c7d6781dd56c32b3dd25d678f9dfc9a829e7fc00
E <e@Es-MacBook-Pro.local>
2020-02-11 12:50:48 -0700

    0.12 array appending

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function startTimer(duration, display) {
     // vars
     var timer = duration,
         minutes, seconds;
-// timer interval count down
-    setInterval(function() {
+    // timer interval count down
+    setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
         // min/sec countdown
@@ -21,9 +21,9 @@ function startTimer(duration, display) {
 }
 
 // timer start function
-window.onload = function() {
+window.onload = function () {
     var loadDisp = 60 * 20,
-    // push timer disp to span
+        // push timer disp to span
         display = document.querySelector('#time');
     startTimer(loadDisp, display);
 };
@@ -44,14 +44,14 @@ var scoreArr = []
 function init() {
     var storedName = JSON.parse(localStorage.getItem("Name"))
     if (storedName !== null)
-    nameArr = storedName
+        nameArr = storedName
     var storedScore = JSON.parse(localStorage.getItem("score"))
     if (storedScore !== null)
-    scoreArr = storedScore
+        scoreArr = storedScore
 }
-function storedNS(){
-    localStorage.setItem("Name", JSON.stringify(nameArr))
-    localStorage.setItem("score", JSON.stringify(scoreArr))
+function storedNS() {
+    localStorage.setItem("Name", JSON.stringify(storedName))
+    localStorage.setItem("score", JSON.stringify(storedScore))
 
 }
 // --------------------------------------------------------------------------------------------------------------
@@ -61,29 +61,30 @@ nxtBtn.addEventListener('click', () => {
     qIndex++
     checkAnswer()
     nextQuest()
+    init()
 })
 // quiz start button function
 function quizStart() {
     // hide start/next btn
     startBtn.classList.add('hide')
     // shuffle the question array
-    qShuffle = questions.sort(()=> Math.random() - 5)
+    qShuffle = questions.sort(() => Math.random() - 5)
     // set question index
     qIndex = 0
     // remove hidden class on question and asnswers
     aContainerEl.classList.remove('hide')
-// finally exe next questions function
+    // finally exe next questions function
     nextQuest()
     // storedNS()
 }
 
 // next question function
-function nextQuest(){
+function nextQuest() {
     resetJumbo()
-    qPop(qShuffle[qIndex])        
-    }
+    qPop(qShuffle[qIndex])
+}
 // question and answer box text replacement
-function qPop(question){
+function qPop(question) {
     // replace q el, with q array index'
     qEl.innerText = question.question
     // for each answer loop to replace answers with new ones in array
@@ -103,45 +104,45 @@ function resetJumbo() {
     nxtBtn.classList.add('hide')
     while (answerBtnsEl.firstChild) {
         answerBtnsEl.removeChild
-        (answerBtnsEl.firstChild)
+            (answerBtnsEl.firstChild)
     }
 }
 // answer selections
-function answerSelec(e){
+function answerSelec(e) {
     var selBtn = e.target
     var correct = selBtn.dataset.correct
     correctStatus(document.body, correct)
     Array.from(answerBtnsEl.children).forEach(button => {
         correctStatus(button, button.dataset.correct)
     })
-    if (qShuffle.length > qIndex +1){
-    nxtBtn.classList.remove('hide')
+    if (qShuffle.length > qIndex + 1) {
+        nxtBtn.classList.remove('hide')
     } else {
         // game finish promt
 
-        nameArr[0] = prompt('enter your initials champ!')
+        storedName = prompt('enter your initials champ!')
         // score amalg
         // highScore = highScoreArr[0] + ":" + score
         // local storage high score saved
-       var scorage = score
-       
-       // console.log(highScore)
-       nameArr.push(name)
-       scoreArr.push(scorage)
-       localStorage.setItem('Name', JSON.stringify(nameArr))
-       localStorage.setItem('score', JSON.stringify(scoreArr))
+        var scorage = score
+
+        // console.log(highScore)
+        nameArr.push(storedName) // TODO
+        scoreArr.push(scorage)
+        localStorage.setItem('Name', JSON.stringify(nameArr))
+        localStorage.setItem('score', JSON.stringify(scoreArr))
         alert(name + ':' + scorage)
         window.location.href = "highScores.html";
     }
 }
 
 function corCount(answer) {
-    if (answer == qShuffle[qIndex].correct){
+    if (answer == qShuffle[qIndex].correct) {
     }
 }
-function correctStatus(el, correct){
+function correctStatus(el, correct) {
     clearCS(el)
-    if(correct){
+    if (correct) {
         el.classList.add('correct')
     } else {
         el.classList.add('wrong')
@@ -149,19 +150,19 @@ function correctStatus(el, correct){
     }
 }
 // check answer function....finally working.----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    function checkAnswer() {
+function checkAnswer() {
     //     alert('test')
     //     document.body.classList.contains('correct')
     //     score++;   
-    if( document.body.className.match('correct') ) { 
+    if (document.body.className.match('correct')) {
         score++
         scoreD.innerText = score
-     }
     }
+}
 // check answer function....finally working.----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
- function clearCS(element) {
+function clearCS(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
@@ -169,45 +170,45 @@ var questions = [
     {
         question: 'Commonly used data types DO NOT include:',
         answers: [
-        {text:'Strings',},
-        {text:'Booleans',},
-        {text:'Alerts', correct:true},
-        {text:'Numbers',}    
-    ]
-},
-{
-    question: 'The condition in an if/else statement is enclosed within___.',
-    answers: [
-    {text:'Quotes',},
-    {text:'Currly Brackets',},
-    {text:'Parentheses', correct:true},
-    {text:'square brackets',}    
-]
-},
-{
-    question: 'Arrays in JavaScript can be used to store___.',
-    answers: [
-    {text:'Numbers and strings', correct:true},
-    {text:'Other Arrays', correct:true},
-    {text:'Booleans', correct:true},    
-]
-},
-{
-    question: 'String values must be enclosed within__ when being assigned to variables.',
-    answers: [
-    {text:'Quotes', correct:true},
-    {text:'Currley Bois',},
-    {text:'Commas',},
-    {text:'Parentheses',}    
-]
-},
-{
-    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-    answers: [
-    {text:'Javascript',},
-    {text:'Terminal/Bash',},
-    {text:'For Loops',},
-    {text:'Console.log', correct:true}    
-]
-},
+            { text: 'Strings', },
+            { text: 'Booleans', },
+            { text: 'Alerts', correct: true },
+            { text: 'Numbers', }
+        ]
+    },
+    {
+        question: 'The condition in an if/else statement is enclosed within___.',
+        answers: [
+            { text: 'Quotes', },
+            { text: 'Currly Brackets', },
+            { text: 'Parentheses', correct: true },
+            { text: 'square brackets', }
+        ]
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store___.',
+        answers: [
+            { text: 'Numbers and strings', correct: true },
+            { text: 'Other Arrays', correct: true },
+            { text: 'Booleans', correct: true },
+        ]
+    },
+    {
+        question: 'String values must be enclosed within__ when being assigned to variables.',
+        answers: [
+            { text: 'Quotes', correct: true },
+            { text: 'Currley Bois', },
+            { text: 'Commas', },
+            { text: 'Parentheses', }
+        ]
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            { text: 'Javascript', },
+            { text: 'Terminal/Bash', },
+            { text: 'For Loops', },
+            { text: 'Console.log', correct: true }
+        ]
+    },
 ]
